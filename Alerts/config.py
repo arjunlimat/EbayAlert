0f868b96a626a36301bf597f2d8b27b73a89521b
Tamilselvan Arjunan <Tamilselvan.Arjunan@gds.ey.com>
2023-06-02 11:48:25 +0530

    adding config.py file

---
+++ Alerts/config.py
@@ -0,0 +1,8 @@
+# config.py
+DEV = {
+    'SECURITY-APPNAME': 'yourappname',
+    'MAILGUN_APIKEY': 'mailgunapi',
+    'EBAY_API_URL': 'EBAY_API_URL',
+    'MAILGUN_SENDER': 'MAILGUN_SENDER',
+    'MAILGUN_API_URL': 'MAILGUN_API_URL',
+}
